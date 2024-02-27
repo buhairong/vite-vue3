5ab479a9b17437d6566ea9c9863e32b22f652ab8
buhairong <buhairong@51cheyaoshi.com>
2024-02-27 16:30:56 +0800

    2-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { defineComponent } from "vue";
 
+import 'styles/index.css'
+import classes from 'styles/test.module.css'
+import 'styles/test.less'
+
 export default defineComponent({
   setup() {
     return () => {
       return (<>
-        <div>hello vue3 jsx</div>
+        <div class="root">hello vue3 jsx</div>
+        <div class="css-variable">css variable</div>
+        <div class="div">css @import</div>
+        <div class={`${classes.moduleClass}`}>css module</div>
+        <div class="div1">css pre-processors</div>
       </>)
     }
   }
